698f9799c2a8d56227a7d1ecf814d89cf6ea9e33
alibekshak <alibekshakiro1@gmail.com>
2024-02-08 13:19:33 +0600

    InterestingLinks

---
+++ InterestingLinks/InterestingLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestingLinksApp.swift
+//  InterestingLinks
+//
+//  Created by Alibek Shakirov on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InterestingLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
